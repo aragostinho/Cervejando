bcd12ac6b757a38ea9c3f14d73c2b02e76bb4b04
Andre Rocha Agostinho <andre@magnadev.com>
2014-12-16 00:32:50 -0200

    Website cervejando - primeira version

---
+++ website/test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace test
+{
+    [Test]
+    public class UnitTest1
+    {
+        [TestFixture]
+        public void TestMethod1()
+        {
+        }
+    }
+}
