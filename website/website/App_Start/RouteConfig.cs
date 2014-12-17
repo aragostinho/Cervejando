289fa44bc19492591b034f9a6a53924ef91631d2
Andre Rocha Agostinho <andre@magnadev.com>
2014-12-17 01:19:54 -0200

    Gateway de compra

--- website/website/App_Start/RouteConfig.cs
+++ website/website/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace website
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //routes.MapMvcAttributeRoutes();
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
